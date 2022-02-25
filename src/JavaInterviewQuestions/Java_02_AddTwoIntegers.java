490c55e8f5314a486dd76238c2651f7b3affca19
Said Demirhan <87076544+SaidDemirhan@users.noreply.github.com>
2022-02-25 20:53:12 +0100

    Update Java_02_AddTwoIntegers.java

--- src/JavaInterviewQuestions/Java_02_AddTwoIntegers.java
+++ src/JavaInterviewQuestions/Java_02_AddTwoIntegers.java
@@ -12,6 +12,18 @@ public class Java_02_AddTwoIntegers {
 		int sum = a+b;
 		
 		System.out.println("Sum of a and b is: "+ sum);
+		
+		
+		//=============================================//
+
+		Scanner scanner=new Scanner(System.in);
+
+		System.out.println("Lütfen 2 sayi giriniz:");
+
+		int sayi1= scanner.nextInt();
+		int sayi2= scanner.nextInt();
+
+		System.out.println("Toplam: "+ (sayi1+sayi2) );
 
 	}
 
