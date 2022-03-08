11e45284265eb1a2e2fa1f5da84f46322bfc329f
said <saiddemirhan@gmail.com>
2022-03-08 12:19:08 +0100

    g

--- src/JavaInterviewQuestions/Java_08_AlphabetVowelOrConsonant.java
+++ src/JavaInterviewQuestions/Java_08_AlphabetVowelOrConsonant.java
@@ -5,46 +5,46 @@ import java.util.Scanner;
 public class Java_08_AlphabetVowelOrConsonant {
 
 	public static void main(String[] args) {
-		
-      //Question: Check whether an alphabet is Vowel or Consonant
+
+		//Question: Check whether an alphabet is Vowel or Consonant
 		// a e i o u
-		
+
 		char ch = 'c';
-		
+
 		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
 			System.out.println(ch + " is vowel");
-		}
-		else {
+		} else {
 			System.out.println(ch + " is consonant");
 		}
-		
+
 		//Second way
 		switch (ch) {
-		case 'a':
-		case 'e':
-		case 'i':
-		case 'o':
-		case 'u':
-			System.out.println(ch + " is vowel");
-			break;
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				System.out.println(ch + " is vowel");
+				break;
 
-		default:
-			System.out.println(ch + " is consonant");
-			break;
+			default:
+				System.out.println(ch + " is consonant");
+				break;
 		}
 
 		//===================================================//
 
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Lütfen bir harf giriniz:");
 
-		char harf= scanner.next().charAt(0);
+		char harf = scanner.next().charAt(0);
 
-		if (harf == 'a'|| harf='e' || harf='i'|| harf=='o' || harf=='u'){
+		if (harf=='a'||harf=='e'||harf=='i'||harf=='u'||harf=='ü'||harf=='o'||harf=='ö') {
 			System.out.println("Girilen rakam ünlü harftir.");
-		}else {System.out.println("Girilen harf ünlü harftir.");
+		} else {
+			System.out.println("Girilen harf ünsüz harftir.");
 
+					}
 	}
-
 }
